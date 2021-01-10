49a2d95386f647748886aee44f3572c4aba085e8
James Finlay <jtfinlay1@gmail.com>
2021-01-09 21:20:34 -0800

    debugging iphone in production

--- components/hooks/useIsApplePhone.ts
+++ components/hooks/useIsApplePhone.ts
@@ -3,6 +3,8 @@
 export default function useIsApplePhone() {
     // Yes, this is dumb
     // But iOS autoplay policies don't allow for autoplay videos on web with audio. So for iOS (and only iOS), playback is muted by default.
+    // Alternative might be to wire up an elaborate key-event when user opens the stories? Per keydown event notes here:
+    // https://webkit.org/blog/6784/new-video-policies-for-ios/
     return [
         'iPad Simulator',
         'iPhone Simulator',
--- components/viewDialog/clippy.tsx
+++ components/viewDialog/clippy.tsx
@@ -15,7 +15,7 @@ export default function Clippy(props: IOwnProps) {
 
     return (
         <Box style={{ width: '100%', height: '100%', display: 'inline-block', position: 'relative'}} >
-            {props.type === FileType.VIDEO && <video controls={isIPhone} src={props.url} autoPlay playsInline loop width={width} height={height} style={{borderRadius: '5px'}}></video>}
+            {props.type === FileType.VIDEO && <video controls={isIPhone} muted={isIPhone} src={props.url} autoPlay playsInline loop width={width} height={height} style={{borderRadius: '5px'}}></video>}
             {props.type === FileType.IMAGE && <img src={props.url} width={width} height={height} style={{borderRadius: '5px'}}/>}
         </Box>
     );
